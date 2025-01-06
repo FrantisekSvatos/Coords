41cdb7ad0768253ccd753c4db213c54e299fbb56
František Svatoš <frantisek.svatos.023@pslib.cz>
2025-01-06 09:11:07 +0100

    pokus

---
+++ Coords/Coord.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coords
+{
+    internal class Coord : IEquatable<Coord>
+    {
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public double Size 
+        { 
+            get 
+            { 
+                return Math.Sqrt(X * X + Y * Y); 
+            } 
+        }
+
+        public double Magnitude 
+        { 
+            get 
+            { 
+                return (X * X + Y * Y); 
+            } 
+        }
+
+        public bool Equals (Coord other)
+        {
+            if (X == other.X && Y == other.Y)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(Coord a, Coord b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Coord a, Coord b)
+        {
+            return !a.Equals(b);
+        }
+        
+        public int CompareTo (Coord other)
+        {
+            return Size.CompareTo (other.Size);
+        }
+
+        public static bool operator >(Coord a, Coord b)
+        {
+            return a.CompareTo(b) > 0;
+        }
+
+        public static bool operator <(Coord a, Coord b)
+        {
+            return a.CompareTo(b) < 0;
+        }
+    }
+}
---
+++ Coords/Program.cs
@@ -0,0 +1,7 @@
+﻿using Coords;
+
+Coord c1 = new Coord(10, 10);
+Coord c2 = new Coord(10, 10);
+
+Console.WriteLine(c1 == c2);
+Console.WriteLine(c2.Equals(c2));
